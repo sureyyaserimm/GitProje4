faddb04fc4e62fdcd8afd588a10386aa9fabc61c
Sureya <sureyyaserimm@gmail.comm>
2023-01-26 09:10:46 -0800

    The fifth task has been done.

--- src/Gun01/School.java
+++ src/Gun01/School.java
@@ -4,5 +4,7 @@ public class School {
     public static void main(String[] args) {
 
         System.out.println("Hello from school");
+
+        System.out.println("Hello from school again, Sorry for bothering you guys :)");
     }
 }
--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -3,7 +3,10 @@ package Gun01;
 public class Student {
     public static void main(String[] args) {
         System.out.println("Hello 1");
+
         System.out.println("Hello 2");
+
         System.out.println("Hello again");
+
     }
 }
